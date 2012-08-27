290da6615eca54d13890ab4ca4bfca6f138bd4d7
Skrynkovskyy Igor <skrynkovskyy@gmail.com>
2012-08-27 18:27:01 +0300

    add README

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import docclass
+import feedfilter
+
+#
+# Naive Bayes classifier
+#
+#cl2=docclass.naivebayes(docclass.getwords)
+#cl2.setdb('test2.db')
+#docclass.sampletrain(cl2)
+
+#
+# Fisher classifier
+#
 
 cl=docclass.fisherclassifier(docclass.getwords)
-cl.setdb('test.db')
+cl.setdb('feed.db')
 docclass.sampletrain(cl)
-
-cl2=docclass.naivebayes(docclass.getwords)
-cl2.setdb('test2.db')
-docclass.sampletrain(cl2)
+feedfilter.read('python_search.xml', cl)
